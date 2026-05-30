b9460815363c5dcd1a49feedf450d93f2626d97d
hujing <1527262047@qq.com>
2026-05-30 14:13:42 +0800

    实现角色的增删改查后端API

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+
+from web.models.character import Character
 from web.models.user import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)  # 传入列表所以,不能删除
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', )
